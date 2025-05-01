02cce2e3202462fb237a1e1d26284f1f211a38a7
Ahmed Shousha <hamada.shousha.2000@gmail.com>
2025-05-01 22:17:25 +0300

    Delete my_model.py

--- my_model.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class TaskItem(models.Model):
-    _name = 'task.item'
-    _description = 'Task Item'
-
-    name = fields.Char(string="Title")
-    description = fields.Text(string="Description")
